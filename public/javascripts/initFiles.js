058202c90df184d64a619c7cbb55592b5a732179
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-01-22 11:54:25 -0500

    Added initFiles to init select input

---
+++ public/javascripts/initFiles.js
@@ -0,0 +1,20 @@
+$.ajax({
+  url:"/miRna",
+  success: function (data) {
+    console.log(data);
+  }
+});
+
+$.ajax({
+  url:"/mRna",
+  success: function (data) {
+    console.log(data);
+  }
+});
+
+$.ajax({
+  url:"/users",
+  success: function (data) {
+    console.log(data);
+  }
+});

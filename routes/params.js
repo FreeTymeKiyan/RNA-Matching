bc07c3826c4ca8eda787bef6d00de693c599ac51
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-01-23 12:12:24 -0500

    Added routes for form params

--- routes/params.js
+++ routes/params.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('form');
+router.post('/', function(req, res, next) {
+  console.log(req.body);
 });
 
 module.exports = router;

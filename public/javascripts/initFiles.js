c3d3e32b46f8cbc49173abbd89222fb991a53761
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-01-23 11:38:21 -0500

    Got file list from server end

--- public/javascripts/initFiles.js
+++ public/javascripts/initFiles.js
@@ -1,20 +1,25 @@
 $.ajax({
   url:"/miRna",
   success: function (data) {
-    console.log(data);
+    for (x in data) {
+      var dotIdx = data[x].lastIndexOf(".");
+      $("select#input1")
+        .append(
+          $("<option>", { value : data[x] }).text(data[x].slice(0, dotIdx))
+        );
+    }
   }
 });
 
 $.ajax({
   url:"/mRna",
   success: function (data) {
-    console.log(data);
-  }
-});
-
-$.ajax({
-  url:"/users",
-  success: function (data) {
-    console.log(data);
+    for (x in data) {
+      var dotIdx = data[x].lastIndexOf(".");
+      $("select#input2")
+        .append(
+          $("<option>", { value : data[x] }).text(data[x].slice(0, dotIdx))
+        );
+    }
   }
 });

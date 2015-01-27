d79304800eb9b9ee30180a674e0ce08ae66adf94
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-01-27 11:57:58 -0500

    Added child process execution in route

--- routes/params.js
+++ routes/params.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var exec = require('child_process').exec;
+var child;
+
+var publicDir = '../public';
+var databaseDir = publicDir + '/database';
+
+var miRNADir = databaseDir + '/miRNA/';
+var mRNADir = databaseDir + '/mRNA/';
+var resDir = databaseDir + '/result/';
+var targetProf1 = 'Targetprofiler_full.txt';
+var targetProf2 = 'targetscan_60_output_Redo.txt.preprocess.out.txt';
+var targetProf3 = 'v5.txt.homo_sapiens.hsa.miRanda.txt';
 
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  var cmd = 'perl ../public/database/scripts/MMiRNA-Plot.pl'
+    + " " + miRNADir + req.body.miRNA 
+    + " " + mRNADir + req.body.mRNA 
+    + " " + resDir + targetProf1
+    + " " + resDir + targetProf2
+    + " " + resDir + targetProf3
+    + " " + req.body.top;
+  console.log(cmd);
+  child = exec('ls', function (err, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (err !== null) {
+      console.log('exec error: ' + err);
+    }
+  });
 });
 
 module.exports = router;

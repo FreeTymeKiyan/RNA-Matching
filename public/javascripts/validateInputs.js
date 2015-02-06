5fce58fe41315c39f2aaadafe71063540652991a
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-02-06 12:06:45 -0500

    Added input validation

--- public/javascripts/validateInputs.js
+++ public/javascripts/validateInputs.js
@@ -1,44 +1,79 @@
 $(document).ready(function () {
-  $("span#email").hide();
-  
-  $('button#submit').click(function () {
-      console.log($('select#input1').val());
-      console.log($('select#input2').val());
-      console.log($('select#top').val());
-      console.log($('select#database').val());
-      console.log($('input#email').val());
-  });
-  
-  $('select#input1').on('change', function (e) {
-    if ($(this).val() == 0) console.log('choose miRNA file');
-  });
-  
-  $('select#input2').on('change', function (e) {
-    if ($(this).val() == 0) console.log('choose miRNA file');
-  });
-  
-  $('select#top').on('change', function (e) {
-    console.log('select top: ' + $(this).val());
-  });
-  
-  $('select#database').on('change', function (e) {
-    console.log('select database: ' + $(this).val());
-  });
+  $("span.glyphicon").hide();
+  $("span.help-block").hide();
   
   $('input#email').on('change', function (e) {
-    console.log("input email: " + $(this).val());
-    // TODO validate email and show state
     var email = $(this).val()
     $('span#email').hide();
-    if (email) {
-      $('div#email').removeClass('has-error');
-      $('div#email').addClass('has-success');
-      $('span#email.glyphicon').show();
+    if (!email) {
+      noEmail();
     } else {
-      $('div#email').removeClass('has-success');
-      $('div#email').addClass('has-error');
-      $('span#email').hide();
-      $('span#email.help-block').show();
+      isValidEmail();
     }
   });
 });
+
+var validateMiRna = function () {
+  var miRna = $('input#input1').val();
+  if (!miRna) {
+    $('#fg1').addClass('has-error');
+    return false;
+  }
+  $('#fg1').removeClass('has-error');
+  $('#fg1').addClass('has-success');
+  console.log(miRna);
+  return true;
+}
+
+var validateMRna = function () {
+  var mRna = $('input#input2').val();
+  if (!mRna) {
+    $('#fg2').addClass('has-error');
+    return false;
+  }
+  $('#fg2').removeClass('has-error');
+  $('#fg2').addClass('has-success');
+  console.log(mRna);
+  return true;
+}
+
+var validateEmail = function () {
+  var email = $('input#email').val();
+  $('span#email').hide();
+  if (!email) {
+    noEmail();
+    return false;
+  }
+  isValidEmail();  
+  console.log(email);
+  return true;
+}
+
+var validateForm = function () {
+  // console.log(validateMiRna() && validateMRna() && validateEmail());
+  var res1 = validateMiRna();
+  var res2 = validateMRna();
+  var res3 = validateEmail();
+  return res1 && res2 && res3;
+}
+
+var noEmail = function () {
+  noInput('div#email', 'span#email.help-block', 'span#email.glyphicon.glyphicon-remove');
+}
+
+var isValidEmail = function () {
+  validInput('div#email', 'span#email.glyphicon.glyphicon-ok');
+}
+
+var noInput = function (div, helpBlock, span) {
+  $(div).removeClass('has-success');
+  $(div).addClass('has-error');
+  $(helpBlock).show();
+  $(span).show();
+}
+
+var validInput = function (div, span) {
+  $(div).removeClass('has-error');
+  $(div).addClass('has-success');
+  $(span).show();
+}

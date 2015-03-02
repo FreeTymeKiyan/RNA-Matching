e376207abdbe431de68f77ac74451d2f52966493
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-03-02 15:16:42 -0500

    Changed to jquery post

--- public/javascripts/queryResult.js
+++ public/javascripts/queryResult.js
@@ -2,25 +2,16 @@ const HALF_MIN = 30 * 1000;
 const ADDR = "http://bioinf1.indstate.edu/result";
 
 var checkResult = function (id) {
-  var request = $.ajax({
-    url: ADDR,
-    type: "POST",
-    data: { id : id },
-    dataType: "json"
-  });
-
-  request.done(function(msg) {
-    if (!msg) {
-      console.log("no msg");
-      return;
-    }
-    if (!msg.hasGenerated) {
-      console.log("not generated");
-      setTimeout(checkResult(id), HALF_MIN);
-    }
-  });
-
-  // request.fail(function(jqXHR, textStatus) {
-  //   alert("Request failed to send: " + textStatus);
-  // });
+  $.post(ADDR, { id : id }, 
+    function (data) {
+      if (!data) {
+        console.log("no data");
+        return;
+      }
+      if (!data.hasGenerated) {
+        console.log("not generated");
+        setTimeout(checkResult(id), HALF_MIN);
+      }
+    }, 
+    dataType: "json");
 };

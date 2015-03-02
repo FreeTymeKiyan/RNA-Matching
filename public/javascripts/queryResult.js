36c014e3cade7ec56c793b485e528f470bbe484c
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-03-02 14:07:42 -0500

    Added auto refresh

---
+++ public/javascripts/queryResult.js
@@ -0,0 +1,25 @@
+const HALF_MIN = 30 * 1000;
+const ADDR = "http://bioinf1.indstate.edu/result";
+
+var checkResult = function (id) {
+  console.log(id);
+  var request = $.ajax({
+    url: ADDR,
+    type: "POST",
+    data: { id : id },
+    dataType: "json"
+  });
+
+  request.done(function(msg) {
+    $( "#log" ).json(msg);
+    if (!msg) return;
+    
+    if (!msg.hasGenerateed) {
+      setTimeout(request, HALF_MIN);
+    }
+  });
+
+  request.fail(function(jqXHR, textStatus) {
+    alert("Request failed to send: " + textStatus);
+  });
+};
--- routes/result.js
+++ routes/result.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 
+/*
+  For download
+*/
 router.get('/', function(req, res, next) {
   var id = req.query.sid;
   if (!id) res.render('download', { id : 0 });
@@ -13,6 +16,19 @@ router.get('/', function(req, res, next) {
     } else {
       res.render('download', { id : id, exists : false });
     }
+  }
+});
+
+/*
+  For check 
+*/
+router.post('/', function (req, res, next) {
+  var id = req.query.sid;
+  var dir = '../public/downloads/' + id;
+  if (fs.existsSync(dir)) {
+    res.render('download', { id : id , exists : true });
+  } else {
+    res.json({hasGenerateed: false});
   }
 });
 

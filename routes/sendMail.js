daf19ae8bbd0aacb094e2c5f1510610d9991c400
FreeTymeKiyan <freetymesunkiyan@gmail.com>
2015-03-02 13:28:31 -0500

    Updated mail content

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,11 +1,15 @@
 var exec = require('child_process').exec;
-// cat Template.txt | mailx -v -s "Your Recent MMiRNA-Tar Result" -r "help@bioinf1.indstate.edu" -a ../public/downloads/1/result.zip freetymesunkiyan@gmail.com
+/*
+ * cat Template.txt | mailx -v -s "Your Recent MMiRNA-Tar Result" -r
+ * "help@bioinf1.indstate.edu" -a ../public/downloads/1/result.zip
+ * freetymesunkiyan@gmail.com
+ */
 
 var buildCommand = function(id, toEmail) {
-  return 'echo "Hi,\n\nYour recent calculation result is in the attachment. You can also download it here: http://bioinf1.indstate.edu/download?sid=' + id + ' Thank you very much for using this tool. Please feel free to give us any feedback.\n\nBest regards,\n\nMMiRNA-Tar Team"'  
-    + ' | ' 
+  return 'echo "Hi,\n\nYour recent calculation result is in the attachment. You can also download it here: http://bioinf1.indstate.edu/download?sid=' + id + ' \n\nIf you have any questions, suggestions, or to report problems, do not reply to this message, write instead to Yongsheng.Bai@indstate.edu.\n\nThank you very much for using this tool.\n\nBest regards,\n\nMMiRNA-Tar Team"'
+    + ' | '
     + 'mailx -s "Your Recent MMiRNA-Tar Result" -r "help@bioinf1.indstate.edu" -a '
-    + "../public/downloads/" + id + "/result.zip " 
+    + "../public/downloads/" + id + "/result.zip "
     + toEmail;
 };
 
@@ -26,4 +30,3 @@ var sendMail = function(id, toEmail) {
 }
 
 exports.sendMail = sendMail;
-
